916dd757c2f215da5361dbc72e3122b9e62c8747
Ricardo Rios <rickrios23@hotmail.com>
2021-11-25 08:20:59 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send('Nenhuma pagina foi encontrada!');
+    res.render('pages/404');
 });
 
 server.listen(process.env.PORT);
